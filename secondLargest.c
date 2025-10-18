a834d48b0d99353a6d6e5b5f2d5bf4015829456f
sachin yadav <sachinyadav@63928.in>
2025-10-18 16:30:00 +0530

    add new paragraph

---
+++ secondLargest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int arr[8] = {24,79,777,93,90,75,80,50};
+    int max = 0 ;
+    for(int i=0;i<=7;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+    printf("%d\n",max);
+
+
+    return 0;
+}
