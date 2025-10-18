fccc85f7803108dd1080ce1d601f70629216ac65
sachin yadav <sachinyadav@63928.in>
2025-10-18 23:37:17 +0530

    add new paragraph

---
+++ function/functionlibrary.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    /*int n ;
+    printf("enter a number : ");
+    scanf("%d",&n);*/
+    int root = sqrt(625);
+    printf("the square root is : %d\n",root);
+   /*int a ;
+    printf("enter a number :");
+    scanf("%d",&a);
+    int b ;
+    printf("enter a number :");
+    scanf("%d",&b);*/
+    int q = pow(5,5);
+    printf("the square value is :%d\n",q);
+
+    return 0;
+}   
