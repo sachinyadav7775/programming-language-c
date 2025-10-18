76edca9c32b1645ea0218894d286e9b02fa3b811
sachin yadav <sachinyadav@63928.in>
2025-10-18 17:26:46 +0530

    add new paragraph

--- basicarray.c
+++ basicarray.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(){
-    /*int arr[5] = {2,4,5,9,3};
+    int arr[5] = {2,4,5,9,3};
     printf("%d\n",arr[0]);
     float Arr[5] = {5.3,3.4,7.8,2.00,6.9};
     printf("%f\n",Arr[1]);
     char ARr[6] = {'a','j','i','?','v','@'};
-    printf("%c\n",ARr[3]);*/
+    printf("%c\n",ARr[3]);
 
     int arr[4];                                                 //= {2,4,7,5,1};
     for(int i=0;i<4;i++){
