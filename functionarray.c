bd039a3e5be087b62809fb299cf3c8dca83fa8c9
sachin yadav <sachinyadav@63928.in>
2025-10-18 17:40:10 +0530

    add new paragraph

---
+++ functionarray.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void fun(int x[]){
+    x[0] = 108;
+    return;
+}
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    printf("%d\n",arr[1]);
+    fun(arr);
+    printf("%d\n",arr[0]);
+    return 0;
+}
