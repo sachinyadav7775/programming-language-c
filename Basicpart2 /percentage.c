9b56e76b69a6bab286e6b314073245a574f538c0
sachin yadav <sachinyadav@63928.in>
2025-10-19 17:19:36 +0530

    add new paragraph

---
+++ Basicpart2 /percentage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float m1 = 97 ; // chemistry marks
+    float m2 = 91 ; // hindi marks
+    float m3 = 78 ; // math marks
+    float m4 = 58 ; // english marks 
+    float m5 = 64 ; // physics marks
+    float percent = (m1 + m2 + m3 + m4 + m5 )/ 5 ;
+    printf("percentage of 5 subjects is : %f",percent);
+
+    return 0;
+}
