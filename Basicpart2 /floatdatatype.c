d59d28d6b2fda0364f897918477199536de8a1e7
sachin yadav <sachinyadav@63928.in>
2025-10-19 17:25:26 +0530

    add new paragraph

---
+++ Basicpart2 /floatdatatype.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    float x = 5 , y = 8 ; 
+    printf("\n%f",x + y );
+    printf("\n%f",x - y );
+    printf("\n%f",x * y );
+    printf("\n%f",x / y );
+    printf("\n%f",y / x );
+
+    float a = 8.14 , b = 7.85 ;
+    printf("\n%f",a + b);
+    printf("\n%f",a - b);
+    printf("\n%f",a * b);
+    printf("\n%f",a / b);
+
+    float e = 6 , g = 5 , d = e / g ;
+    printf("\n%f",d);
+    float w = 9 , i = 22 ;
+    float u = 9 / 22; 
+    printf("\n%f",u);
+
+    return 0;
+    
+}
