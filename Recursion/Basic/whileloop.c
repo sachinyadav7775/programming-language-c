9815369bd6047890b767900d00bd6aaebec780ac
sachin yadav <sachinyadav@63928.in>
2025-10-19 16:44:52 +0530

    add new paragraph

---
+++ Recursion/Basic/whileloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    int i = 1;
+            while(i<n){
+        printf("%d ",i);
+    i = i + 1;
+    }
+    return 0;
+}
