464b97f7776c2d497e7c52b98e6b2feb5e6afd13
sachin yadav <sachinyadav@63928.in>
2025-10-19 16:27:45 +0530

    add new paragraph

---
+++ Recursion/stairpathcount.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int stair(int n){
+    if(n<=3) return n;
+    /*if(n==1) return 1 ;
+    if(n==2) return 2 ;
+    if(n==3) return 3 ;*/
+    return stair(n-1)+stair(n-2)+stair(n-3);
+}
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    int ways = stair(n);
+    printf("%d\n",ways);
+     return 0;
+}
