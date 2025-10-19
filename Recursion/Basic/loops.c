9713a4679da52d702bda23820289727bd820ced1
sachin yadav <sachinyadav@63928.in>
2025-10-19 16:40:47 +0530

    add new paragraph

---
+++ Recursion/Basic/loops.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+int n ;
+printf("Enter a number : ");
+scanf("%d",&n);
+int i ;
+for(i=1;i<=n;i=i+3){
+printf("hello sky\n");
+}
+printf("%d",i);
+int i ;
+for(i=1;i<=n;i++){
+if(i%2==0)
+printf("%d ",i);
+}
+int i ;
+for(i=1;i<=n;i++){
+if(i%3==0)
+printf("%d ",i);
+}
+return 0;
+}
+
