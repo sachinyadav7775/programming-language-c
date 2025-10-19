c0270cfbb5aa16d07055217f00112dee03df6721
sachin yadav <sachinyadav@63928.in>
2025-10-19 16:49:54 +0530

    add new paragraph

---
+++ Recursion/Basic/areaperimeter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int l ;
+  printf("enter length:");
+  scanf("%d",&l);
+  int b ;
+  printf("enter breadth:");
+  scanf("%d",&b);
+  int a = l*b ;
+  int p = 2 * (l+ b );
+  if (a>p){
+    printf("area is freater than perineter");
+  }  
+  else{
+    printf("area  not freater than perineter");
+  }
+  return 0;
+}
