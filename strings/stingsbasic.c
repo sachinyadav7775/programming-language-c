96f642234d4fbb670ad5e1c0b4c75391cf3c0e80
sachin yadav <sachinyadav@63928.in>
2025-10-21 16:04:16 +0530

    add new paragraph

--- strings/stingsbasic.c
+++ strings/stingsbasic.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
-
+    char arr[] = {'H','e','l','l','o','!'};
+    // char ch = '\0'; // null charcter
+    // int x = 0;
+    //char a = (char)x; // a -> '\0'
+    // printf("%c",a);
+    int i = 0;
+    while(arr[i]!='!'){
+        printf("%c ",arr[i]);
+        i++;
+    }
     return 0;
 }
