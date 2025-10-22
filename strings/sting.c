ca8f3522e812a83bb5c6eba4a649ad632b69a4df
sachin yadav <sachinyadav@63928.in>
2025-10-22 14:43:39 +0530

    add new paragraph

---
+++ strings/sting.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    /*char arr[] = "Hello sky";
+    int i = 0;
+    while(i<9){
+        printf("%c",arr[i]);
+        i++;
+    }
+    printf("\n");*/
+
+    char Arr[] = "Hello sky";
+    int i = 0;
+    while(Arr[i]!='\0'){
+        printf("%c",Arr[i]);
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
