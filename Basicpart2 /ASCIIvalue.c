83b09f5d2aff77b7a7e6ebce43fe29a2778816ac
sachin yadav <sachinyadav@63928.in>
2025-10-22 16:51:37 +0530

    add new paragraph

---
+++ Basicpart2 /ASCIIvalue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch = 'A';                          // ASCII = A -> 65
+    printf("%d\n",ch);                      // ASCII = a -> 97
+    char Ch = 'a';
+    printf("%c\n",Ch);
+    
+    return 0;
+
+}
