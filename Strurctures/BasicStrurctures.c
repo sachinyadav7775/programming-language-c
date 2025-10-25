630821b98751dfabe143e57d2af58c6294926a4f
sachin yadav <sachinyadav63928@gmail.com>
2025-10-25 16:12:20 +0530

    add new paragraph

---
+++ Strurctures/BasicStrurctures.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    struct pokemon{
+        int hp;
+        int speed;
+        int attack;
+        char tier; // S , G , A , B , C , D
+    }sky;
+    struct pokemon pikachu;
+    // printf("Enter attack of pikachu : ");
+    // scanf("%d",&pikachu.attack);       // user se input lena 
+    pikachu.attack = 70;
+    pikachu.hp = 80;
+    pikachu.speed = 95;
+    pikachu.tier = 'G';
+    printf("%d\n",pikachu.attack);
+    
+    struct pokemon meena ;
+    // printf("Enter attack of meena : ");
+    // scanf("%d",&meena.speed);       // user se input lena 
+    meena.attack = 70;
+    meena.hp = 80;
+    meena.speed = 95;
+    meena.tier = 'S';
+    printf("%c\n",meena.tier);
+
+    return 0;
+}
