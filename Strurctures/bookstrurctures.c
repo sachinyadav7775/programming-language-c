6b16c3d9d922558a3059376011dfbb1a2756f80f
sachin yadav <sachinyadav63928@gmail.com>
2025-10-26 11:15:55 +0530

    add new pragraph

--- Strurctures/bookstrurctures.c
+++ Strurctures/bookstrurctures.c
@@ -6,15 +6,25 @@ int main(){
         int noOfpages;
         float price;
     }a,b,c;
+
     a.noOfpages = 100;
     a.price = 411.5;
     strcpy(a.name,"Secret Seven");
+
     b.noOfpages = 250;
-    b.price = 499.5;
+    b.price = 750.50;
     strcpy(b.name,"Famous Five");
+
+    c.noOfpages = 5005;
+    c.price = 299.558;
+    strcpy(c.name,"Famous Two");
+
     printf("%d\n",b.noOfpages);
     printf("%f\n",a.price);
     printf("%s\n",a.name);
+    printf("%s\n",c.noOfpages);
+    printf("%d\n",b.name);
+    printf("%f\n",c.price);
 
     return 0;
 }
