c17293d440c056c4143f4c7499525466654fbfd2
sachin yadav <sachinyadav63928@gmail.com>
2025-10-28 06:26:33 +0530

    add new paragraph

--- Strurctures/Question.c
+++ Strurctures/Question.c
@@ -9,22 +9,62 @@ int main(){
         float average;
     }cricketer;
 
-    // 3 cricketer 
+    // 3 cricketer                                                        // [1]
 
-    cricketer arr[4];
-    for(int i=0;i<4;i++){
-        scanf("%s",arr[i].firstname);
-        scanf("%s",arr[i].lastname);
-        scanf("%d",&arr[i].age);
-        scanf("%d",&arr[i].noOfmatches);
-        scanf("%f",&arr[i].average);
+    cricketer Arr[3];
+    for(int i=0;i<3;i++){
+        scanf("%s",Arr[i].firstname);
+        scanf("%s",Arr[i].lastname);
+        scanf("%d",&Arr[i].age);
+        scanf("%d",&Arr[i].noOfmatches);
+        scanf("%f",&Arr[i].average);
     }
-    for(int i=0;i<4;i++){
-        printf("Name : %s %s\n",arr[i].firstname,arr[i].lastname);
-        printf("Age : %d\n",arr[i].age);
-        printf("Number of maeches : %d\n",arr[i].noOfmatches);
-        printf("Average runs : %f\n\n",arr[i].average);
-    }
+
+    for(int i=0;i<3;i++){
+        printf("Name : %s %s\n",Arr[i].firstname,Arr[i].lastname);
+        printf("Age : %d\n",Arr[i].age);
+        printf("Number of maeches : %d\n",Arr[i].noOfmatches);
+        printf("Average runs : %f\n\n",Arr[i].average);
+   } 
     
+    typedef struct car{
+        char name[30];
+        char number[40];                                                    // [2]
+        int speed ;
+        float average;
+        char tier;
+    }car;
+
+    car arr[3];
+
+    // 3 car 
+
+    arr[0].speed = 210;
+    arr[0].average = 90.5;
+    arr[0].tier = 'A';
+    strcpy(arr[0].name,"Fortunr");
+    strcpy(arr[0].number ,"IND UP 50 AK 7580");
+    
+    arr[1].speed = 340;
+    arr[1].average = 288.5;
+    arr[1].tier = 'B';
+    strcpy(arr[1].name,"BMW");
+    strcpy(arr[1].number ,"IND UP 50 BS 5400");
+    
+    arr[2].speed = 180;
+    arr[2].average = 85.5;
+    arr[2].tier = 'C';
+    strcpy(arr[2].name,"Thar");
+    strcpy(arr[2].number , "IND UP 50 MK 4801");
+
+    for(int i=0;i<3;i++){
+        printf("Car Name : %s\n",arr[i].name);
+        printf("Number Plate : %s\n",arr[i].number);
+        printf("tier : %c\n",arr[i].tier);
+        printf("Speed : %d\n",arr[i].speed);
+        printf("Average : %f\n\n",arr[i].average);
+    }
+
     return 0;
+    
 }
